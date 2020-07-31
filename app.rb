a89df6a1cdf9a72f836b6dad9b81ab4ef15c1ceb
Micah Banschick <micha.ban@gmail.com>
2020-07-31 16:20:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ class App < Sinatra::Base
   get '/multiply/:num1/:num2' do
     @num1 = params[:num1].to_i 
     @num2 = params[:num2].to_i
-    binding.pry
+    # binding.pry
     product = @num1 * @num2 
   end
 
