af58000333cd3b186cbbd81c37edfa0307aef4ac
Micah Banschick <micha.ban@gmail.com>
2020-07-31 16:09:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,5 +14,20 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get '/medicines/:id' do
+    @medicine = all_the_medicines.select do |medicine|
+      medicine.id == params[:id]
+    end.first
+    erb :'/medicines/show.html'
+  end
+  
+  get '/goodbye/:name' do
+    @name = params[:name]
+    "Goodbye, #{@name}."
+  end
+  
+  get '/multiply/:num1/:num2' do
+    @num1 = p
+  end
 
 end
